8d016a16052580261660e3aefa6957443715d38b
maytom2016 <qwerty448qwe@163.com>
2023-05-25 23:27:08 +0800

    添加项目文件。

---
+++ UAC-Control/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Button1 = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        OpenFileDialog1 = New OpenFileDialog()
+        Button2 = New Button()
+        ListView1 = New ListView()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        ToolStripMenuItem1 = New ToolStripMenuItem()
+        Button3 = New Button()
+        Button4 = New Button()
+        Label2 = New Label()
+        ToolStripMenuItem2 = New ToolStripMenuItem()
+        ContextMenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(45, 85)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(91, 31)
+        Button1.TabIndex = 0
+        Button1.Text = "选择程序"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(35, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(114, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "应用程序位置："
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(144, 27)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(550, 27)
+        TextBox1.TabIndex = 3
+        TextBox1.Text = "c:\to\path\exam.ple"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(161, 86)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(162, 29)
+        Button2.TabIndex = 4
+        Button2.Text = "永久以普通权限启动"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ListView1
+        ' 
+        ListView1.ContextMenuStrip = ContextMenuStrip1
+        ListView1.Location = New Point(35, 135)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(739, 286)
+        ListView1.TabIndex = 6
+        ListView1.UseCompatibleStateImageBehavior = False
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.ImageScalingSize = New Size(20, 20)
+        ContextMenuStrip1.Items.AddRange(New ToolStripItem() {ToolStripMenuItem1, ToolStripMenuItem2})
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(211, 80)
+        ' 
+        ' ToolStripMenuItem1
+        ' 
+        ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        ToolStripMenuItem1.Size = New Size(210, 24)
+        ToolStripMenuItem1.Text = "删除项"
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(352, 85)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(175, 29)
+        Button3.TabIndex = 7
+        Button3.Text = "永久以管理员权限启动"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(554, 85)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 8
+        Button4.Text = "刷新列表"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(678, 89)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(81, 20)
+        Label2.TabIndex = 9
+        Label2.Text = "总共：N项"
+        ' 
+        ' ToolStripMenuItem2
+        ' 
+        ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        ToolStripMenuItem2.Size = New Size(210, 24)
+        ToolStripMenuItem2.Text = "手动修改项值"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(791, 449)
+        Controls.Add(Label2)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(ListView1)
+        Controls.Add(Button2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "UAC-Control"
+        ContextMenuStrip1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+End Class
---
+++ UAC-Control/Form1.vb
@@ -0,0 +1,142 @@
+﻿Imports System.Buffers
+
+Public Class Form1
+    Public Const Regpath = "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers"
+    Public Const SRegpath = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            TextBox1.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim str, str1 As String
+        str = TextBox1.Text
+        str1 = str.Substring(str.Length - 3, 3)
+        If str1 = "exe" Then
+            My.Computer.Registry.SetValue(Regpath, TextBox1.Text, "RUNASINVOKER")
+            MsgBox("设置" + TextBox1.Text + "成功")
+            ListView1.Clear()
+            Call loadlistview()
+        Else
+            MsgBox("您所选文件的并非可执行程序，请重新选择")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'listview的初始化
+        ListView1.View = View.Details '不加这句添加不了
+        ListView1.GridLines = True  '格子
+        ListView1.FullRowSelect = True '整行选
+        ListView1.Columns.Clear()   '清空
+        Call loadlistview()
+    End Sub
+    Public Sub AutoCtlSize(ByVal inObj As Control)     '自动调整控件大小 
+        If inObj Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim DeskTopSize As Size = System.Windows.Forms.SystemInformation.PrimaryMonitorSize
+        Dim FormSize = Me.Size
+
+        If ((inObj.Name.IndexOf("ListView") <> -1)) Then
+            inObj.Size = New Size(Int(FormSize.Width - 70), Int(FormSize.Height - 200))
+        End If
+        If ((inObj.Name.IndexOf("TextBox") <> -1)) Then
+            inObj.Size = New Size(Int(FormSize.Width - 200), inObj.Height)
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim str, str1 As String
+        str = TextBox1.Text
+        str1 = str.Substring(str.Length - 3, 3)
+        If str1 = "exe" Then
+            My.Computer.Registry.SetValue(Regpath, TextBox1.Text, "~ RUNASADMIN")
+            MsgBox("设置" + TextBox1.Text + "成功")
+            ListView1.Clear()
+            Call loadlistview()
+        Else
+            MsgBox("您所选文件的并非可执行程序，请重新选择")
+        End If
+    End Sub
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        Dim i As Integer
+        i = Me.ListView1.SelectedIndices(0)
+        If ((MsgBox("确认是否删除" + ListView1.Items(i).SubItems(0).Text, vbYesNo, "UAC-Control")) = 6) Then
+            'Debug.Print(ListView1.Items(i).SubItems(0).Text)
+            My.Computer.Registry.CurrentUser.OpenSubKey(SRegpath, True).DeleteValue(ListView1.Items(i).SubItems(0).Text, False)
+        End If
+        ListView1.Clear()
+        Call loadlistview()
+    End Sub
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        Dim str As String = InputBox("输入项值", "UAC-Control", 1)
+        Dim i As Integer = Me.ListView1.SelectedIndices(0)
+        If (str <> "") Then
+            My.Computer.Registry.SetValue(Regpath, ListView1.Items(i).SubItems(0).Text, str)
+            MsgBox("修改项" + ListView1.Items(i).SubItems(0).Text + "值成功！")
+        End If
+        ListView1.Clear()
+        Call loadlistview()
+    End Sub
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        Call AutoCtlSize(Me.ListView1)
+        Call AutoCtlSize(Me.TextBox1)
+        If ListView1.Columns.Count > 0 Then
+            ListView1.Columns(0).Width = ListView1.ClientSize.Width - ListView1.Columns(1).Width - ListView1.Columns(2).Width
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'ListView1.Columns.Clear()
+        ListView1.Clear()
+        Call loadlistview()
+    End Sub
+    Private Sub loadlistview()
+        Dim dic As Dictionary(Of String, String) = New Dictionary(Of String, String)
+        dic.Add("WIN", "WINDOWS旧版兼容模式")
+        dic.Add("RUNASADMIN", "管理员权限运行")
+        dic.Add("HIGHDPIAWARE", "高DPI感知")
+        dic.Add("RUNASINVOKER", "以普通权限运行")
+
+        ' ListView表头
+        ListView1.Columns.Add("程序位置", ListView1.ClientSize.Width - 350, HorizontalAlignment.Left)
+        ListView1.Columns.Add("键值", 170, HorizontalAlignment.Left)
+        ListView1.Columns.Add("状态", 180, HorizontalAlignment.Left)
+
+        Dim num, i As Integer
+        Dim keyname() As String
+        Dim readValue
+        Dim trykey As String = ""
+        num = My.Computer.Registry.CurrentUser.OpenSubKey(SRegpath).GetValueNames.Count
+        Label2.Text = "总共：" + Str(num) + "项"
+        ReDim keyname(num)
+        keyname = My.Computer.Registry.CurrentUser.OpenSubKey(SRegpath).GetValueNames
+        For i = 0 To (num - 1)
+            Dim item As New ListViewItem()
+            item.Text = keyname(i)
+            readValue = My.Computer.Registry.GetValue(Regpath, keyname(i), Nothing)
+            item.SubItems.Add(readValue)
+            readValue = Replace(readValue, " ", "")
+            readValue = Replace(readValue, "~", "")
+            readValue = Replace(readValue, "^", "")
+            If InStr(1, readValue, "WIN", CompareMethod.Text) > 0 Then
+                item.SubItems.Add("WINDOWS旧版兼容模式")
+            Else
+                dic.TryGetValue(readValue, trykey)
+                If (trykey <> "") Then
+                    item.SubItems.Add(trykey)
+                Else
+                    item.SubItems.Add("未知")
+                End If
+            End If
+            ListView1.Items.Add(item)
+        Next i
+        If ListView1.Columns.Count > 0 Then
+            ListView1.Columns(0).Width = ListView1.ClientSize.Width - ListView1.Columns(1).Width - ListView1.Columns(2).Width
+        End If
+    End Sub
+
+End Class
